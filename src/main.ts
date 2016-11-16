c065de8ffbb8c2f1fc42afe466eccbf8c9fbf3c9
Reece Bennett <reece.bennett1@gmail.com>
2016-11-16 23:29:11 +0000

    Add EaselJS and some basic graphics

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,30 @@
 function byID(id: string): HTMLElement {
   return document.getElementById(id);
 }
-
-let page = {
-  status: {
-    money: byID("statusMoney"),
-    in: byID("statusPotsIn"),
-    out: byID("statusPotsOut"),
-    total: byID("statusPotsTot"),
-    day: byID("statusDay")
-  }
-}
-
+  
 let g = new Game();
 
-function update() {
-  page.status.money.innerText = g.money.toString();
-  page.status.in.innerText = g.potsIn.toString();
-  page.status.out.innerText = g.potsOut.toString();
-  page.status.total.innerText = g.pots.toString();
-  page.status.day.innerText = g.day.toString();
-}
+function init() {
+  let stage = new createjs.Stage("canvas");
+  let sWidth = (<HTMLCanvasElement>stage.canvas).width;
+  let sHeight = (<HTMLCanvasElement>stage.canvas).height;
 
-update();
+  let sceneSea = new createjs.Container();
 
-function act(action: string) {
-  switch(action) {
-    case "potIn": g.movePotIn(); break;
-    case "potOut": g.movePotOut(); break;
-    case "buyPot": g.buyPots(1); break;
-    case "doTurn": 
-      g.doTurn(); 
-      if (g.wasFine) { console.log("Was fine"); }
-      else { console.log("Was stormy"); }
-      break;
-  }
-  
-  update();
+  let sky = new createjs.Shape();
+  sky.graphics.lf(["#3498DB", "#FAD7A0"], [0, 1], 0, 0, 0, sHeight*.75).drawRect(0, 0, sWidth, sHeight);
+
+  let mountains = new createjs.Shape();
+  mountains.graphics.beginFill("#229954")
+    .mt(0, sHeight*.75)
+    .lt(sWidth*.4, sHeight*.6)
+    .lt(sWidth*.6, sHeight*.7)
+    .lt(sWidth*.8, sHeight*.65)
+    .lt(sWidth, sHeight*.75)
+    .lt(sWidth, sHeight)
+    .lt(0, sHeight);
+
+  sceneSea.addChild(sky, mountains);
+  stage.addChild(sceneSea);
+  stage.update();
 }
